a6ed3000f40f3551e87df80d37eb1286337f9908
asciimoo <asciimoo@gmail.com>
2013-08-23 09:45:54 +0200

    [enh][mod] sinswarm mods

--- swarms/sin.py
+++ swarms/sin.py
@@ -2,22 +2,29 @@
 
 from math import sin
 
+from random import randint
+
 from swarm import Agent, World
 
 class SinAgent(Agent):
 
-    def init(self):
+    def init(self, world):
         self.rounds = 0.0
+        self.max_age = randint(1, 100)
+        self.world = world
+        self.color = 'FF00FFFF'
 
 
     def act(self):
         self.x += sin(self.rounds+int(self.name.split('_')[1]))
         self.y -= sin(self.rounds)
         self.rounds += .1
+        if self.rounds > self.max_age:
+            self.world.remove(self)
 
 if __name__ == '__main__':
     world = World()
-    [world.add(SinAgent()) for x in range(1000)]
+    [world.add(SinAgent(world=world)) for x in range(1000)]
     print world
     for i in range(1000):
         print world.genNext()
